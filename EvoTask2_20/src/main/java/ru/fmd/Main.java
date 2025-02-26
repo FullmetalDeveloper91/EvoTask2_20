20692e5cbc5d778fbe772d8812602877550287b0
Andrey Chudotvorov <lonelywolf796@gmail.com>
2025-02-26 12:29:39 +0700

    task 2_20 is complete

---
+++ EvoTask2_20/src/main/java/ru/fmd/Main.java
@@ -0,0 +1,28 @@
+package ru.fmd;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+        Random rnd = new Random();
+        Integer[] array = new Integer[20];
+        Map<Integer, Integer> frequency = new HashMap<>();
+
+        for(int i = 0; i < array.length; i++){
+            array[i] = rnd.nextInt(1,16);
+            System.out.printf("%d ", array[i]);
+
+            if(frequency.containsKey(array[i]))
+                frequency.put(array[i], frequency.get(array[i])+1);
+            else
+                frequency.put(array[i], 1);
+        }
+        System.out.println();
+
+        for (Integer i : frequency.keySet()) {
+            System.out.printf("Число '%d' встречается %d раз\n", i, frequency.get(i));
+        }
+    }
+}
